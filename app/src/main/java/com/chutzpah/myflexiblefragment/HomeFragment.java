cb59d312e9ef6df5d516dcedca1bab440f8b299f
avtara <avtarakhrisna1@gmail.com>
2019-12-09 15:11:30 +0700

    Initial commit

---
+++ app/src/main/java/com/chutzpah/myflexiblefragment/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.chutzpah.myflexiblefragment;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment implements View.OnClickListener{
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCategory = view.findViewById(R.id.btn_category);
+        btnCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+
+    }
+
+
+}
+
